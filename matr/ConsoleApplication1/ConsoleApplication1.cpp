b1f85a8ef04a59aeb54744bb5034fd9ef3736724
nawal addou <nawal.addou@gmail.com>
2013-05-15 23:51:39 +0100

    essaySousrepertoire

---
+++ matr/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+// ConsoleApplication1.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+ 
+void dessineRectangle(int l, int h)
+{
+    for(int ligne(0); ligne < h; ligne++)
+    {
+        for(int colonne(0); colonne < l; colonne++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+ 
+int main()
+{
+    int largeur, hauteur;
+    cout << "Largeur du rectangle : ";
+    cin >> largeur;
+    cout << "Hauteur du rectangle : ";
+    cin >> hauteur;
+     
+    dessineRectangle(largeur, hauteur);
+    return 0;
+}
+
